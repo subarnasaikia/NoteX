b4c598a9acabeae98ca05bd72224be905a005e31
Subarna Saikia <saikiasubarna74@gmail.com>
2025-04-26 04:16:08 +0530

    added eslint.config.js and update few prettier settings.

---
+++ backend/eslint.config.js
@@ -0,0 +1,27 @@
+// eslint.config.js
+import js from "@eslint/js";
+import tseslint from "typescript-eslint";
+import prettier from "eslint-config-prettier";
+
+export default [
+    js.configs.recommended,
+    ...tseslint.configs.recommended,
+    prettier,
+    {
+        files: ["**/*.ts"],
+        languageOptions: {
+            parser: tseslint.parser,
+            parserOptions: {
+                ecmaVersion: "latest",
+                sourceType: "module",
+            },
+        },
+        rules: {
+            "@typescript-eslint/no-explicit-any": "warn",
+            "@typescript-eslint/no-unused-vars": [
+                "warn",
+                { argsIgnorePattern: "^_" },
+            ],
+        },
+    },
+];
