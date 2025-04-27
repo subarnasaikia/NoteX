8f97ae535cf7a70cd6c8904fe12f025da0e02d18
Subarna Saikia <saikiasubarna74@gmail.com>
2025-04-27 18:45:47 +0530

    Add: connected mongodb database.

---
+++ backend/src/db/connect.db.ts
@@ -0,0 +1,36 @@
+import mongoose, { ConnectOptions, Mongoose } from "mongoose";
+import { DB_NAME } from "../constants.js";
+import { loadConfig } from "../config/loadConfig.js";
+import { AppConfig } from "../config/type.js";
+import { ApiError } from "../utils/ApiError.js";
+
+const config: AppConfig = loadConfig();
+
+const connectDB = async (): Promise<void> => {
+    try {
+        const connectionInstance: Mongoose = await mongoose.connect(
+            `${config.database.mongodb_uri}/${DB_NAME}`,
+            {} as ConnectOptions,
+        );
+
+        console.log(
+            `MongoDB connected host: ${connectionInstance.connection.host}`,
+        );
+        console.log(
+            `MongoDB connected name: ${connectionInstance.connection.name}`,
+        );
+    } catch (error: unknown) {
+        if (error instanceof Error || error instanceof ApiError) {
+            console.error(`MongoDB connection failed: ${error.message}`);
+            throw new ApiError(
+                500,
+                `MongoDB connection failed: ${error.message}`,
+            );
+        } else {
+            console.error("MongoDB connection failed: Unknown error", error);
+            throw new ApiError(500, "MongoDB connection failed: Unknown error");
+        }
+    }
+};
+
+export { connectDB };
--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,9 +1,21 @@
 import { loadConfig } from "./config/loadConfig.js";
-import { app } from "./app.js";
 import { AppConfig } from "./config/type.js";
+import { app } from "./app.js";
+import { connectDB } from "./db/connect.db.js";
 
 const config: AppConfig = loadConfig();
 
-app.listen(config.server.port, () => {
-    console.log(`Server running on port : ${config.server.port}`);
-});
+connectDB()
+    .then(() => {
+        app.on("error", (error: unknown) => {
+            console.error("server error: ", error);
+            process.exit(1);
+        });
+        app.listen(config.server.port, () => {
+            console.log(`Server is running on port ${config.server.port}`);
+        });
+    })
+    .catch((error) => {
+        console.log("Error connecting to the database", error);
+        process.exit(1);
+    });
