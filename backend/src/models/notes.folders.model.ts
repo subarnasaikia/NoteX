39e8a692213daf2ce306a29715f3383b31953a4e
Subarna Saikia <saikiasubarna74@gmail.com>
2025-05-01 14:59:53 +0530

    Add: Add folder model schema

---
+++ backend/src/models/notes.folders.model.ts
@@ -0,0 +1,48 @@
+import mongoose, { Schema, Document, Model, ObjectId } from "mongoose";
+
+export interface IFolder extends Document {
+    _id: ObjectId;
+    folderName: string;
+    description: string;
+    hex_color: string;
+    userId: ObjectId;
+    parentFolderId?: ObjectId;
+    createdAt: Date;
+    updatedAt: Date;
+}
+
+const folderSchema = new Schema<IFolder>(
+    {
+        folderName: {
+            type: String,
+            default: "new folder",
+            required: [true, "Folder name is required!"],
+            trim: true,
+        },
+        description: {
+            type: String,
+            default: "",
+            trim: true,
+        },
+        hex_color: {
+            type: String,
+            default: "#000000",
+        },
+        userId: {
+            type: Schema.Types.ObjectId,
+            ref: "User",
+            required: [true, "User ID is required!"],
+        },
+        parentFolderId: {
+            type: Schema.Types.ObjectId,
+            ref: "Folder",
+        },
+    },
+    { timestamps: true },
+);
+
+const FolderModel: Model<IFolder> = mongoose.model<IFolder>(
+    "Folder",
+    folderSchema,
+);
+export default FolderModel;
