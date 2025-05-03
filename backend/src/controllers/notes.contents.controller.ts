4b7c1ffe6b8091c0d145f851e69c93b33778f9b8
Subarna Saikia <saikiasubarna74@gmail.com>
2025-05-03 21:25:00 +0530

    write controler for contents

---
+++ backend/src/controllers/notes.contents.controller.ts
@@ -0,0 +1,169 @@
+import { asyncHandler } from "../utils/asyncHandler.js";
+import { ApiError } from "../utils/ApiError.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
+import { Request, Response } from "express";
+import ContentModel, { IContents } from "../models/notes.contents.model.js";
+
+// create new content
+const createContent = asyncHandler(
+    async (req: Request, res: Response): Promise<void> => {
+        if (!req.user) {
+            throw new ApiError(401, "User not authenticated!");
+        }
+        // TODO(subarna): checking if user's email is verified
+
+        const {
+            title,
+            hex_color,
+            body,
+            parentFolderId,
+            tags,
+        }: {
+            title: string;
+            hex_color: string;
+            body: IContents["body"];
+            parentFolderId: string;
+            tags?: string[];
+        } = req.body;
+
+        if (!title) {
+            throw new ApiError(400, "Title is required!");
+        }
+
+        if (!body) {
+            throw new ApiError(400, "Body is required!");
+        }
+        const newContent: IContents = await ContentModel.create({
+            title,
+            hex_color,
+            body,
+            parentFolderId,
+            userId: req.user?._id,
+            tags,
+        });
+
+        res.status(201).json(
+            new ApiResponse(200, newContent, "Content created successfully!"),
+        );
+    },
+);
+
+// fetch or read the  contents as page
+
+// fetch content using contentId
+const fetchContentUsingId = asyncHandler(
+    async (req: Request, res: Response): Promise<void> => {
+        if (!req.user) {
+            throw new ApiError(401, "User not authenticated!");
+        }
+        // TODO(subarna): checking if user's email is verified
+
+        const { contentId } = req.params;
+
+        if (!contentId) {
+            throw new ApiError(400, "Content ID is required!");
+        }
+
+        const content: IContents | null = await ContentModel.findOne({
+            _id: contentId,
+            userId: req.user?._id,
+        });
+
+        if (!content) {
+            throw new ApiError(404, "Content not found!");
+        }
+
+        res.status(200).json(
+            new ApiResponse(200, content, "Content fetched successfully!"),
+        );
+    },
+);
+
+// update content
+const updateContentUsingId = asyncHandler(
+    async (req: Request, res: Response): Promise<void> => {
+        if (!req.user) {
+            throw new ApiError(401, "User not authenticated!");
+        }
+        // TODO(subarna): checking if user's email is verified
+
+        const { contentId } = req.params;
+
+        if (!contentId) {
+            throw new ApiError(400, "Content ID is required!");
+        }
+
+        const {
+            title,
+            hex_color,
+            body,
+            parentFolderId,
+            tags,
+        }: {
+            title: string;
+            hex_color: string;
+            body: IContents["body"];
+            parentFolderId: string;
+            tags?: string[];
+        } = req.body;
+
+        const content: IContents | null = await ContentModel.findOneAndUpdate(
+            {
+                _id: contentId,
+                userId: req.user?._id,
+            },
+            {
+                title,
+                hex_color,
+                body,
+                parentFolderId,
+                tags,
+            },
+            { new: true },
+        );
+
+        if (!content) {
+            throw new ApiError(404, "Content not found!");
+        }
+
+        res.status(200).json(
+            new ApiResponse(200, content, "Content updated successfully!"),
+        );
+    },
+);
+
+// delete content
+const deleteContentUsingId = asyncHandler(
+    async (req: Request, res: Response): Promise<void> => {
+        if (!req.user) {
+            throw new ApiError(401, "User not authenticated!");
+        }
+        // TODO(subarna): checking if user's email is verified
+
+        const { contentId } = req.params;
+
+        if (!contentId) {
+            throw new ApiError(400, "Content ID is required!");
+        }
+
+        const content: IContents | null = await ContentModel.findOneAndDelete({
+            _id: contentId,
+            userId: req.user?._id,
+        });
+
+        if (!content) {
+            throw new ApiError(404, "Content not found!");
+        }
+
+        res.status(200).json(
+            new ApiResponse(200, content, "Content deleted successfully!"),
+        );
+    },
+);
+
+export {
+    createContent,
+    fetchContentUsingId,
+    updateContentUsingId,
+    deleteContentUsingId,
+};
--- backend/src/routes/notes.contents.route.ts
+++ backend/src/routes/notes.contents.route.ts
@@ -1,7 +1,18 @@
 import { Router } from "express";
 import { verfiyJWT } from "../middlewares/auth.middleware.js";
+import {
+    createContent,
+    fetchContentUsingId,
+    updateContentUsingId,
+    deleteContentUsingId,
+} from "../controllers/notes.contents.controller.js";
 
 const router: Router = Router();
 router.use(verfiyJWT);
+router.route("/create-content").post(createContent);
+router.route("/fetch-content/:contentId").get(fetchContentUsingId);
+router.route("/update-content/:contentId").patch(updateContentUsingId);
+router.route("/delete-content/:contentId").delete(deleteContentUsingId);
+// TODO(subarna): add search content using search query
 
 export default router;
