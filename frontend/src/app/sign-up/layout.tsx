e65082d9d335958ca88ec4732561a458eb802957
gautamhazarika <gautamhazarika01@gmail.com>
2025-05-19 15:48:05 +0530

    added sign-up

--- frontend/src/app/sign-up/layout.tsx
+++ frontend/src/app/sign-up/layout.tsx
@@ -1,33 +1,29 @@
 import type { Metadata } from "next";
 import { ThemeProvider } from "@/components/theme-provider"
-import "./globals.css";
-
 
 export const metadata: Metadata = {
   title: "NoteX",
   description: "AI note taking app.",
 };
 
-
-
-export default function RootLayout({ children }: Readonly<{
-    children: React.ReactNode;
-  }>) {
+export default function RootLayout({
+  children,
+}: Readonly<{ children: React.ReactNode }>) {
   return (
-    <>
-      <html lang="en" suppressHydrationWarning>
-        <head />
-        <body>
-          <ThemeProvider
-            attribute="class"
-            defaultTheme="system"
-            enableSystem
-            disableTransitionOnChange
-          >
-            {children}
-          </ThemeProvider>
-        </body>
-      </html>
-    </>
-  )
+    <html lang="en" suppressHydrationWarning>
+      <head />
+      <body>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <div className="min-h-svh flex items-center justify-center p-4">
+            <div className="w-full max-w-xs">{children}</div>
+          </div>
+        </ThemeProvider>
+      </body>
+    </html>
+  );
 }
