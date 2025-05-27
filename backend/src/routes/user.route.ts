ec008a46d9d1f4509c59af74cf5fde67875e86c1
Subarna Saikia <saikiasubarna74@gmail.com>
2025-05-27 15:47:45 +0530

    fix: error in me route.

--- backend/src/routes/user.route.ts
+++ backend/src/routes/user.route.ts
@@ -14,6 +14,6 @@ router.route("/register").post(registerUser);
 router.route("/login").post(loginUser);
 router.route("/logout").get(verfiyJWT, logoutUser);
 router.route("/refresh-token").get(refreshAccessToken);
-router.route("/me").get(getCurrentUser);
+router.route("/me").get(verfiyJWT, getCurrentUser);
 
 export default router;
